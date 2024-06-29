d2e7fb06e9b55409328dfe2fbcfb87bd643242f9
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-06-29 17:47:17 +0100

    chore: Update server middleware and MongoDB connection configuration

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors");
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -9,21 +10,22 @@ const URI = process.env.URI;
 const app = express();
 
 // Middleware to parse JSON and URL-encoded bodies
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Routes
 app.use('/api', userRoute);
 
 // Connect to MongoDB
+
 mongoose.connect(URI)
     .then(() => {
-        console.log('Connected to MongoDB!');
-        // Start server after successful MongoDB connection
         app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        });
+            console.log(`connected to database & Server is running on port: ${port}`);
+        })
     })
-    .catch((err) => {
-        console.error('Failed to connect to MongoDB', err);
-    });
+    .catch((error) => {
+        console.log(error);
+    })
+    ;
