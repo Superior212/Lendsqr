b13e58a3e576c59c276ca4f42401ec59ddbaa295
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-06-29 17:38:23 +0100

    refactor: Connect to MongoDB and improve middleware for JSON and URL-encoded bodies

--- server/index.js
+++ server/index.js
@@ -8,21 +8,25 @@ const URI = process.env.URI;
 
 const app = express();
 
-// Middleware to parse JSON
+// Middleware to parse JSON and URL-encoded bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api', userRoute);
 
-
-
-mongoose.connect(URI)
+// Connect to MongoDB
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => {
-        console.log('Connected!')
+        console.log('Connected to MongoDB!');
+        // Start server after successful MongoDB connection
         app.listen(port, () => {
-            console.log(`Server is running on port ${port}`)
-        })
-    }).catch((err) => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch((err) => {
         console.error('Failed to connect to MongoDB', err);
     });
